4ad40ef56bbfe503e24bad04fecc6bf6fe8274ab
bx17229305 <146851029+bx17229305@users.noreply.github.com>
2024-07-06 12:45:51 +0800

    1
    1

---
+++ template/二维树状数组/code.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+using namespace std;
+int n,k;
+static const  int V = 5501;
+static const int H = 502;
+static const int N = 1e4 + 1;
+int tree[V][H];
+int arr[N];
+
+int getmax(int x,int y)
+{
+    int res = INT_MIN;
+    for(int i = x; i >= 1; i ^= i & -i)
+    {
+        for(int j = y; j >= 1; j ^= j & -j)
+        {
+            res = max(res,tree[i][j]);
+        }
+    }
+    return res;
+}
+
+void update(int x,int y,int val)
+{
+    for(int i = x; i < V; i += i & -i)
+    {
+        for(int j = y; j < H; j += j & -j)
+        {
+            tree[i][j] = max(val,tree[i][j]);
+        }
+    }
+}
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin >> n >> k;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    int v,dp;
+    int res = INT_MIN;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = k; j >= 0; j--)
+        {
+            v = arr[i] + j;
+            dp = getmax(v,j + 1) + 1;
+            update(v,j + 1,dp);
+            res = max(res,dp);
+        }
+    }
+    cout << res;
+}
